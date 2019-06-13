c0a9ddc603ebdcb68ce07bfde314f6d02cb361a4
chaibingyang <51656735+chaibingyang@users.noreply.github.com>
2019-06-13 22:21:50 +0800

    第二次作业
    于2019,6,12

---
+++ task01.py
@@ -0,0 +1,10 @@
+achievement = int(input("小明的成绩是："))
+if achievement == 100:
+    print("爸爸：给你买车")
+if 100 > achievement >= 90:
+    print("妈妈：给你买mp4")
+elif 60 <= achievement <= 90:
+    print("妈妈：给你买本参考书")
+else:
+    print("妈妈：奖励大嘴巴子")
+
